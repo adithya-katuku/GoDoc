54f4b3a3d41d262fa143512841650155dfeef9d3
Adithya <adithya.beehyv@gmail.com>
2025-03-05 13:06:52 +0530

    Backend setup

---
+++ Backend/src/main/java/com/godoc/service/GoDocApplication.java
@@ -0,0 +1,13 @@
+package com.godoc.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoDocApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/godoc/service/GoDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godoc.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
